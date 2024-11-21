5dcf21067d683a37e77b6740e2f72cf93f357b01
YeibiBlund <brunocue92@gmail.com>
2024-11-21 02:02:20 +0100

    sl

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -2,10 +2,9 @@
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i; 
+	unsigned int i;
 
-	i = 0; 
-
+	i = 0;
 	while (i < n && s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
@@ -21,13 +20,13 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	return (0);
 }
 
-int	main()
+int	main(void)
 {
-	printf("%d\n", ft_strncmp("Hola", "Hola", 4));      // Caso 1
-	printf("%d\n", ft_strncmp("Hola", "Hula", 2));      // Caso 2
-	printf("%d\n", ft_strncmp("Hola", "Holanda", 4));   // Caso 3
-	printf("%d\n", ft_strncmp("Hola", "Hula", 4));      // Caso 4
-	printf("%d\n", ft_strncmp("Hola", "HolaMundo", 8)); // Caso 5
-	printf("%d\n", ft_strncmp("Hola", "", 4));          // Caso 6
-	return 0;
+	printf("%d\n", ft_strncmp("Hola", "Hola", 4));
+	printf("%d\n", ft_strncmp("Hola", "Hula", 2));
+	printf("%d\n", ft_strncmp("Hola", "Holanda", 4));
+	printf("%d\n", ft_strncmp("Hola", "Hula", 4));
+	printf("%d\n", ft_strncmp("Hola", "HolaMundo", 8));
+	printf("%d\n", ft_strncmp("Hola", "", 4));
+	return (0);
 }
