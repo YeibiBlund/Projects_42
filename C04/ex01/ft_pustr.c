b7a7283108d3ecb23bf6ab5ea6e925aec3909255
YeibiBlund <brunocue92@gmail.com>
2024-11-25 01:46:41 +0100

    Actualización de cambios

---
+++ C04/ex01/ft_pustr.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
+}
+
+/*int	main(void)
+{
+	char	str[] = ("Hola mundo.");
+	ft_putstr(str);
+	return (0);
+}*/
